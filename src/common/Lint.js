bed6b20d205fcf116de854cfe3ce2f0a81e4d24b
glennsl <glenn@slotte.net>
2018-01-01 07:55:39 +0100

    refactored lints to support dynamic messages

--- src/common/Lint.js
+++ src/common/Lint.js
@@ -1,84 +1,98 @@
 #!/usr/bin/env node
 'use strict';
 
-var Curry  = require("bs-platform/lib/js/curry.js");
-var Rebase = require("@glennsl/rebase/src/Rebase.bs.js");
+var Curry        = require("bs-platform/lib/js/curry.js");
+var Rebase       = require("@glennsl/rebase/src/Rebase.bs.js");
+var Js_primitive = require("bs-platform/lib/js/js_primitive.js");
 
-var lints_000 = /* tuple */[
-  "Missing description",
-  (function ($$package) {
-      return +($$package.description.trim() === "");
-    })
-];
+function lints_000($$package) {
+  var match = +($$package.description.trim() === "");
+  if (match !== 0) {
+    return /* Some */["Missing description"];
+  } else {
+    return /* None */0;
+  }
+}
 
 var lints_001 = /* :: */[
-  /* tuple */[
-    "Missing readme",
-    (function ($$package) {
-        return +($$package.readme.trim() === "");
-      })
-  ],
+  (function ($$package) {
+      var match = +($$package.readme.trim() === "");
+      if (match !== 0) {
+        return /* Some */["Missing readme"];
+      } else {
+        return /* None */0;
+      }
+    }),
   /* :: */[
-    /* tuple */[
-      "Short readme",
-      (function ($$package) {
-          var l = $$package.readme.trim().length;
-          if (l > 0) {
-            return +(l < 400);
-          } else {
-            return /* false */0;
-          }
-        })
-    ],
+    (function ($$package) {
+        var l = $$package.readme.trim().length;
+        var match = +(l > 0 && l < 400);
+        if (match !== 0) {
+          return /* Some */["Short readme"];
+        } else {
+          return /* None */0;
+        }
+      }),
     /* :: */[
-      /* tuple */[
-        "Missing license",
-        (function ($$package) {
-            return +($$package.license == null);
-          })
-      ],
+      (function ($$package) {
+          var match = +($$package.license == null);
+          if (match !== 0) {
+            return /* Some */["Missing license"];
+          } else {
+            return /* None */0;
+          }
+        }),
       /* :: */[
-        /* tuple */[
-          "Missing keywords",
-          (function ($$package) {
-              return +(Rebase.$$Array[/* length */16]($$package.keywords) === 0);
-            })
-        ],
+        (function ($$package) {
+            var match = +(Rebase.$$Array[/* length */16]($$package.keywords) === 0);
+            if (match !== 0) {
+              return /* Some */["Missing keywords"];
+            } else {
+              return /* None */0;
+            }
+          }),
         /* :: */[
-          /* tuple */[
-            "Missing repository url",
-            (function ($$package) {
-                return +($$package.repositoryUrl == null);
-              })
-          ],
+          (function ($$package) {
+              var match = +($$package.repositoryUrl == null);
+              if (match !== 0) {
+                return /* Some */["Missing repository url"];
+              } else {
+                return /* None */0;
+              }
+            }),
           /* :: */[
-            /* tuple */[
-              "Missing homepage url",
-              (function ($$package) {
-                  return +($$package.homepageUrl == null);
-                })
-            ],
+            (function ($$package) {
+                var match = +($$package.homepageUrl == null);
+                if (match !== 0) {
+                  return /* Some */["Missing homepage url"];
+                } else {
+                  return /* None */0;
+                }
+              }),
             /* :: */[
-              /* tuple */[
-                "Missing issues url",
-                (function ($$package) {
-                    return +($$package.issuesUrl == null);
-                  })
-              ],
+              (function ($$package) {
+                  var match = +($$package.issuesUrl == null);
+                  if (match !== 0) {
+                    return /* Some */["Missing issues url"];
+                  } else {
+                    return /* None */0;
+                  }
+                }),
               /* :: */[
-                /* tuple */[
-                  "Readme > 10k bytes",
-                  (function ($$package) {
-                      return +(Rebase.$$String[/* length */1]($$package.readme) > 10000);
-                    })
-                ],
+                (function ($$package) {
+                    var match = +(Rebase.$$String[/* length */1]($$package.readme) > 10000);
+                    if (match !== 0) {
+                      return /* Some */["Readme > 10k bytes"];
+                    } else {
+                      return /* None */0;
+                    }
+                  }),
                 /* :: */[
-                  /* tuple */[
-                    "Deprecated",
-                    (function ($$package) {
-                        return 1 - +($$package.deprecated == null);
-                      })
-                  ],
+                  (function ($$package) {
+                      return Rebase.Option[/* map */0]((function (message) {
+                                    return "Deprecated - " + message;
+                                  }), Js_primitive.null_undefined_to_opt($$package.deprecated));
+                    }),
                   /* [] */0
                 ]
               ]
@@ -96,14 +110,9 @@ var lints = /* :: */[
 ];
 
 function lintPackage($$package) {
-  return Rebase.List[/* map */0](Rebase.Option[/* getOrRaise */17], Rebase.List[/* filter */10](Rebase.Option[/* isSome */13], Rebase.List[/* map */0]((function (param) {
-                        var match = Curry._1(param[1], $$package);
-                        if (match !== 0) {
-                          return /* Some */[param[0]];
-                        } else {
-                          return /* None */0;
-                        }
-                      }), lints)));
+  return Rebase.List[/* filterMap */18]((function (lint) {
+                return Curry._1(lint, $$package);
+              }), lints);
 }
 
 exports.lints       = lints;
--- src/common/Lint.re
+++ src/common/Lint.re
@@ -1,19 +1,39 @@
 open Rebase;
 
 let lints = [
-  ("Missing description", package => package##description |> Js.String.trim == ""),
-  ("Missing readme", package => package##readme |> Js.String.trim == ""),
-  ("Short readme", package => { let l = package##readme |> Js.String.trim |> Js.String.length; l > 0 && l < 400 }),
-  ("Missing license", package => Js.Nullable.test(package##license)),
-  ("Missing keywords", package => Array.length(package##keywords) == 0),
-  ("Missing repository url", package => Js.Nullable.test(package##repositoryUrl)),
-  ("Missing homepage url", package => Js.Nullable.test(package##homepageUrl)),
-  ("Missing issues url", package => Js.Nullable.test(package##issuesUrl)),
-  ("Readme > 10k bytes", package => String.length(package##readme) > 10000),
-  ("Deprecated", package => !Js.Nullable.test(package##deprecated)),
+  package => package##description |> Js.String.trim == ""
+    ? Some("Missing description") : None,
+
+  package => package##readme |> Js.String.trim == ""
+    ? Some("Missing readme") : None,
+
+  package => {
+    let l = package##readme |> Js.String.trim |> Js.String.length;
+    l > 0 && l < 400
+      ? Some("Short readme") : None
+  },
+
+  package => Js.Nullable.test(package##license)
+    ? Some("Missing license") : None,
+
+  package => Array.length(package##keywords) == 0
+    ? Some("Missing keywords") : None,
+
+  package => Js.Nullable.test(package##repositoryUrl)
+    ? Some("Missing repository url") : None,
+
+  package => Js.Nullable.test(package##homepageUrl)
+    ? Some("Missing homepage url") : None,
+
+  package => Js.Nullable.test(package##issuesUrl)
+    ? Some("Missing issues url") : None,
+
+  package => String.length(package##readme) > 10000
+    ? Some("Readme > 10k bytes") : None,
+
+  package => package##deprecated |> Js.toOption
+                                 |> Option.map(message => "Deprecated - " ++ message)
 ];
 
 let lintPackage: Package.t => list(string) = package =>
-  lints |> List.map(((message, test)) => test(package) ? Some(message) : None)
-        |> List.filter(Option.isSome)
-        |> List.map(Option.getOrRaise);
+  lints |> List.filterMap(lint => lint(package));
